1f08538525b1e901ad7e1a8bff0dd235d6f87fef
qkonstantin <kuznetsov.ks@edu.spbstu.ru>
2019-11-04 23:37:08 +0300

    test

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -304,7 +304,13 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    for (i in list.indices) {
+        for (j in i + 1 until list.size)
+            if (list[i] + list[j] == number) return Pair(i,j)
+    }
+    return Pair(-1,-1)
+}
 
 /**
  * Очень сложная
